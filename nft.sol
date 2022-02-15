ca6e94066f75a62c7e9f21acbe327af837105ecd
David Michael Akers <1138499+davidmichaelakers@users.noreply.github.com>
2022-02-15 14:33:11 -0500

    removed public mint option, removed hashlips royalty, removed paused

--- nft.sol
+++ nft.sol
@@ -1,18 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
 
-// Amended by HashLips
-/**
-    !Disclaimer!
-    These contracts have been used to create tutorials,
-    and was created for the purpose to teach people
-    how to create smart contracts on the blockchain.
-    please review this code on your own before using any of
-    the following code for production.
-    HashLips will not be liable in any way if for the use 
-    of the code. That being said, the code has been tested 
-    to the best of the developers' knowledge to work as intended.
-*/
-
 pragma solidity >=0.7.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
@@ -23,19 +10,15 @@ contract NFT is ERC721Enumerable, Ownable {
 
   string public baseURI;
   string public baseExtension = ".json";
-  uint256 public cost = 0.05 ether;
   uint256 public maxSupply = 10000;
   uint256 public maxMintAmount = 100;
-  bool public paused = false;
-  mapping(address => bool) public whitelisted;
-
+  
   constructor(
     string memory _name,
     string memory _symbol,
     string memory _initBaseURI
   ) ERC721(_name, _symbol) {
     setBaseURI(_initBaseURI);
-    // mint(msg.sender, 20);
   }
 
   // internal
@@ -46,16 +29,9 @@ contract NFT is ERC721Enumerable, Ownable {
   // public
   function mint(address _to, uint256 _mintAmount) public onlyOwner {
     uint256 supply = totalSupply();
-    require(!paused);
     require(_mintAmount > 0);
     require(_mintAmount <= maxMintAmount);
     require(supply + _mintAmount <= maxSupply);
-
-    // if (msg.sender != owner()) {
-        // if(whitelisted[msg.sender] != true) {
-        //   require(msg.value >= cost * _mintAmount);
-        // }
-    // }
 
     for (uint256 i = 1; i <= _mintAmount; i++) {
       _safeMint(_to, supply + i);
@@ -94,10 +70,6 @@ contract NFT is ERC721Enumerable, Ownable {
   }
 
   //only owner
-  function setCost(uint256 _newCost) public onlyOwner {
-    cost = _newCost;
-  }
-
   function setmaxMintAmount(uint256 _newmaxMintAmount) public onlyOwner {
     maxMintAmount = _newmaxMintAmount;
   }
@@ -110,31 +82,8 @@ contract NFT is ERC721Enumerable, Ownable {
     baseExtension = _newBaseExtension;
   }
 
-  function pause(bool _state) public onlyOwner {
-    paused = _state;
-  }
- 
- function whitelistUser(address _user) public onlyOwner {
-    whitelisted[_user] = true;
-  }
- 
-  function removeWhitelistUser(address _user) public onlyOwner {
-    whitelisted[_user] = false;
-  }
-
   function withdraw() public payable onlyOwner {
-    // This will pay HashLips 5% of the initial sale.
-    // You can remove this if you want, or keep it in to support HashLips and his channel.
-    // =============================================================================
-    (bool hs, ) = payable(0x943590A42C27D08e3744202c4Ae5eD55c2dE240D).call{value: address(this).balance * 5 / 100}("");
-    require(hs);
-    // =============================================================================
-    
-    // This will payout the owner 95% of the contract balance.
-    // Do not remove this otherwise you will not be able to withdraw the funds.
-    // =============================================================================
     (bool os, ) = payable(owner()).call{value: address(this).balance}("");
     require(os);
-    // =============================================================================
   }
 }
